740032bc2056b1ee900d5e5d3c9e8d5381f4e2e7
Milos <milos.codes@gmail.com>
2024-01-13 21:52:47 +0100

    response js
    find out how to create an url from the file image. by using createObjectUrl method... and to get elementbyId that has the picture that is hardcoded.

--- 4-response.js
+++ 4-response.js
@@ -16,24 +16,21 @@ let obj = {
 
 export function getData() {
   //
+
   fetch(imgstr)
-    .then((resp) => {
-      if (!resp.ok) throw new Error('invalid');
-      return resp.blob(); //binary large object images, video, audio, fonts.
-
-      resp.text(); //for text, html, and xml files, and css, and js.
-      resp.json(); //for json files
-    })
-    .then((blob) => {
-      console.log(blob); //blob is a chunk of memory on users computer
-      let url = URL.createObjectURL(blob);
-      let img = document.getElementById('pic');
-      img.src = url;
-    })
-    .catch(console.warn);
-
+  .then((resp)=> {
+    if(!response.ok) throw new Error('invalid');
+    return resp.blob(); //Binnary large object its not text, its video, or something.
+  })
+  .then(blob=> {
+    console.log(blob); //blob is chunk of memory.
+    let url = URL.createObjectURL(blob);
+    let img = document.getElementById('pic');
+    img.src = url;
+  })
+  .catch(console.warn)
   let jsonstring = JSON.stringify(obj);
-  // console.log(jsonstring);
+  console.log(jsonstring)
   let file = new File([jsonstring], 'mydata.json', { type: 'application/json' });
 
   let response = new Response(file, {
@@ -45,7 +42,5 @@ export function getData() {
       'x-steve': 'starts with x for a custom header name',
     },
   });
-
-  // console.log(response.headers.get('content-type'));
-  // console.log(response.headers.get('content-length'));
+  console.log(response);
 }

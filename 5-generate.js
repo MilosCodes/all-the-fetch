5b8baeeb13f55ca192bcf4a00ca66d71eb65deba
Milos <milos.codes@gmail.com>
2024-01-14 21:50:54 +0100

    making generating list
    making generating list

--- 5-generate.js
+++ 5-generate.js
@@ -14,29 +14,7 @@ export function getData() {
       if (!response.ok) throw new Error('invalid');
       return response.json();
     })
-    .then((dataArray) => {
-      //
-      list.innerHTML = dataArray
-        .map(({ uid, first_name, last_name }) => {
-          return `<li class="listitem" data-uid="${uid}">
-            <p>${first_name}</p>
-            <p>${last_name}</p>
-          </li>`;
-        })
-        .join('');
-    })
-    .catch(console.warn);
-
-  fetch(textstr)
-    .then((response) => {
-      if (!response.ok) throw new Error('invalid');
-      return response.text();
-    })
-    .then((txt) => {
-      header.innerHTML += `<h2>${txt}</h2>`;
-    })
-    .catch(console.warn);
-
+   
   fetch(imgstr)
     .then((resp) => {
       if (!resp.ok) throw new Error('invalid');

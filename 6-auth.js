aa32ae73707c74658216e743d214824078ec8ed5
Milos <milos.codes@gmail.com>
2024-01-16 20:08:50 +0100

    api keys
    making two possibilities one by api key, the second by my made api key with x-api-key property

--- 6-auth.js
+++ 6-auth.js
@@ -5,35 +5,27 @@ export function getData() {
   //Where can we pass it to the server - querystring, headers, cookies
   //controlling when cookies and credentials are passed to a server
   //CSP meta tags and headers
-
-  let str = 'http://127.0.0.1:3000/?name=value&steve=griffith';
-  let url = new URL(str); //url.search
-  let sp = url.searchParams;
-  sp.append('hello', 'world');
-  sp.append('api-key', 'kajshdfkahjsdfkjhsdfkahsdfkjksdjhfksjdh');
-  // document.cookie('')
-
+  let str = 'http://127.0.0.1:3000/?name=value&misha=glisha';
+  let url = new URL(str); //url.search will be that string, or there is a url search param.
+  let searchParam = url.searchParams;
+  searchParam.append('hello', 'world');
+  searchParam.append('api-key', 'sadasldkasl;dklas');
   let h = new Headers();
   // h.append('content-type', 'application/json')
-  // h.append('origin', 'https://cia.org')
-  h.append('x-api-key', 'kajshdfkahjsdfkjhsdfkahsdfkjksdjhfksjdh'); //API key
-  h.append('Authorization', 'Bearer kajshdfkahjsdfkjhsdfkahsdfkjksdjhfksjdh'); //JWT
-  //Forbidden Header Names
-
+  h.append('blah', 'asdaf', 'whatdefuq');
+  h.append('x-api-key','sadasldkasl') //API KEY that i've made cuz of x-
+  h.append('Authorization', 'Bearer sadasldkasl') // <- How you send a JSON web TOKEN.
+  //Zabranjeni Header Imena postoje.
   let request = new Request(url, {
     method: 'GET',
     headers: h,
-    cache: 'default',
-    credentials: 'same-origin',
   });
-
   fetch(request)
-    .then((response) => {
-      if (!response.ok) throw new Error('invalid');
-      return response.text();
-    })
-    .then((txt) => {
-      console.log(txt);
-    })
-    .catch(console.warn);
+  .then(response=> {
+    if(response.ok) throw Error('invlid');
+    return response.text();
+  })
+  .then(txt=> {
+    return txt.text();
+  }).catch(console.warn);
 }
